5349128760e8afb728be09cb58a1d19384d11cee
student <student@pyclassvm>
2016-08-07 09:09:49 -0400

    Adding our website project to git.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website Project Example
+
+This website is part of a Johns Hopkins class that uses Python to collect and analyze data. The website showcases all the skills learned in the class.
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/results')
+def results():
+	return render_template('results.html')
+
+@app.route('/researchquestion')
+def researchquestion():
+	return render_template('researchquestion.html')
+
+@app.route('/methodology')
+def methodology():
+	return render_template('methodology.html')
+
+@app.route('/projectdescription')
+def projectdescription():
+	return render_template('projectdescription.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
